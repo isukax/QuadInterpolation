3d33a5a1504b4ce390b2b807e8ad080b89f30d85
isuka <12695335+isukax@users.noreply.github.com>
2020-03-15 13:10:19 +0900

    initial commit

---
+++ Assets/CreateQuad.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CreateQuad : MonoBehaviour
+{
+    public bool doExtraInterpolation;
+    public Color mColor;
+
+    Vector4 extraColorData;
+
+    Vector4 ToV4(Color col)
+    {
+        return new Vector4(col.r, col.b, col.b, col.a);
+    }
+
+    void Update()
+    {
+        Mesh mesh = new Mesh();
+        
+        mesh.vertices = new Vector3[]
+        {
+            new Vector3(-1, -1, 0),
+            new Vector3(1, -1, 0),
+            new Vector3(1, 1, 0),
+            new Vector3(-1, 1, 0),
+        };
+
+        mesh.triangles = new[] { 0, 2, 1, 0, 3, 2 };
+
+        // Diagonal
+        mesh.colors = new Color[]
+        {
+            mColor,
+            //new Color(1, 1, 1, 1),
+            new Color(1, 1, 1, 1),
+            new Color(1, 0, 0, 1),
+            new Color(1, 1, 1, 1),
+        };
+
+        //mesh.colors = new Color[]
+        //{
+        //    //new Color(1, 1, 1, 1),
+        //    new Color(1, 1, 1, 1),
+        //    mColor,
+        //    new Color(1, 1, 1, 1),
+        //    new Color(1, 0, 0, 1),
+        //};
+
+        // Calculate extra data for interpolation
+        extraColorData = -ToV4(mesh.colors[0]) + ToV4(mesh.colors[1]) - ToV4(mesh.colors[2]) + ToV4(mesh.colors[3]);
+
+        // Set barycentric per vertex
+        mesh.uv2 = new Vector2[]
+        {
+            new Vector2(1, 0),
+            new Vector2(0, 0),
+            new Vector2(0, 1),
+            new Vector2(0, 0),
+        };
+
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+        mesh.RecalculateTangents();
+
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        Material mat = GetComponent<Renderer>().material;
+        mat.SetVector("_ExtraColorData", extraColorData);
+        if (doExtraInterpolation)
+        {
+            mat.EnableKeyword("DO_INTERP");
+        }
+        else
+        {
+            mat.DisableKeyword("DO_INTERP");
+        }
+    }
+}
---
+++ Assets/VertexColor.shader
@@ -0,0 +1,64 @@
+﻿Shader "Unlit/VertexColor"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+		[Toggle(USE_EXTRA_INTERPOLATION)] _UseExtraInterpolation("Use ExtraInterpolation", Float) = 0
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+			#pragma shader_feature DO_INTERP
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+				float4 color : COLOR;
+				float2 barycentric : TEXCOORD1;
+            };
+
+            struct v2f
+            {
+				float2 uv : TEXCOORD0;
+				float4 color : TEXCOORD1;
+				float2 barycentric : TEXCOORD2;
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+			float4 _ExtraColorData;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				o.color = v.color;
+				o.barycentric = v.barycentric;
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                fixed4 col = tex2D(_MainTex, i.uv);
+#ifdef DO_INTERP
+			return i.color + i.barycentric.x * i.barycentric.y * _ExtraColorData;
+#else
+			return i.color;
+#endif
+            }
+            ENDCG
+        }
+    }
+}
